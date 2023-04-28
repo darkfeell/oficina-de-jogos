59dad3aadfdc2ef1e849e1a07caa9aa18c630d3f
Raphael César Queiroz do Nascimento <queiroz.raphael@academico.ifrn.edu.br>
2023-04-28 16:20:41 -0300

    sla 2

--- Underground/Assets/Scripts/player.cs
+++ Underground/Assets/Scripts/player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,24 @@ public class player : MonoBehaviour
 {
 
     public Vector2 speed = new Vector2(10, 0);
+    public bool isGrounded;
+    public int jumpForce;
 
     
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 
     // Update is called once per frame
@@ -25,6 +38,11 @@ public class player : MonoBehaviour
         movement *= Time.deltaTime;
         
         transform.Translate((movement));
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
 
 
     }
